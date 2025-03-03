38517cd06d870a118a1e62aed42e7b6dfa3c04a3
mohank1234 <your-email@example.com>
2025-03-03 21:07:30 +0530

    Initial commit

---
+++ src/main/java/Pages/Naukriprofileupdate.java
@@ -0,0 +1,71 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.util.concurrent.TimeUnit;
+
+public class Naukriprofileupdate 
+{
+        public static void NakuriChange() throws InterruptedException  {
+    	// Automatically setup ChromeDriver
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver =new ChromeDriver();
+
+        try {
+            // Browser settings
+            driver.manage().deleteAllCookies();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+            // Open Naukri and login
+            driver.get("https://www.naukri.com/");
+            driver.findElement(By.xpath("//a[@title='Jobseeker Login']")).click();
+            Thread.sleep(5000);
+
+            driver.findElement(By.xpath("//input[@placeholder='Enter your active Email ID / Username']"))
+                  .sendKeys("Username/Email");
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//input[@placeholder='Enter your password']"))
+                  .sendKeys("Password");
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//button[text()='Login']")).click();
+            Thread.sleep(5000);
+
+            // Navigate to profile edit page
+            driver.findElement(By.xpath("//div[@class='view-profile-wrapper']//a[@href='/mnjuser/profile']")).click();
+            Thread.sleep(10000);
+
+            // Click on the edit option
+            driver.findElement(By.xpath("//em[text()='editOneTheme']")).click();
+            Thread.sleep(5000);
+
+            // Locate the name input field
+            WebElement nameField = driver.findElement(By.xpath("//input[@placeholder='Enter Your Name']"));
+            String currentName = nameField.getAttribute("value");
+
+            // Toggle name value
+            if (currentName.equals("Krishnna Mohan B")) {
+                nameField.clear();
+                nameField.sendKeys("Krishnna Mohan Batthini");
+            } else if (currentName.equals("Krishnna Mohan Batthini")) {
+                nameField.clear();
+                nameField.sendKeys("Krishnna Mohan B");
+            }
+
+            // Save changes
+            driver.findElement(By.id("saveBasicDetailsBtn")).click();
+
+            System.out.println("Profile name updated successfully!");
+
+        } finally {
+            // Close the browser
+            Thread.sleep(5000);
+            driver.quit();
+        }
+    }
+}
---
+++ src/test/java/Tests/NakuriUpdateTest.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+import Pages.Naukriprofileupdate;
+
+public class NakuriUpdateTest 
+{
+	@Test
+   static void Nakuriupdate() throws Exception
+   {
+	   Naukriprofileupdate.NakuriChange();
+   }
+}

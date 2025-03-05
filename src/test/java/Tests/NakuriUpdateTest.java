c784f14d3315252e542e3abb037ab2e9e2c39232
mohank1234 <your-email@example.com>
2025-03-05 22:22:59 +0530

    added @Before test

--- src/test/java/Tests/NakuriUpdateTest.java
+++ src/test/java/Tests/NakuriUpdateTest.java
@@ -1,11 +1,19 @@
 package Tests;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import Pages.Naukriprofileupdate;
 
 public class NakuriUpdateTest 
 {
+	@BeforeTest
+    public void setup() throws Exception {
+        // Kill existing Chrome processes before starting the test
+        Runtime.getRuntime().exec("pkill -f chrome");
+        Thread.sleep(2000); // Small delay to ensure process is killed
+    }
+
 	@Test
    static void Nakuriupdate() throws Exception
    {

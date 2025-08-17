2d3ff99fe7d1feba935afe55288cac16fe775c54
Krishna Mohan B <95032471+mohank1234@users.noreply.github.com>
2025-08-17 21:35:58 +0530

    Updatd for CI and headless in NakuriUpdateTest.java

--- src/test/java/Tests/NakuriUpdateTest.java
+++ src/test/java/Tests/NakuriUpdateTest.java
@@ -1,14 +1,20 @@
 package Tests;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import Pages.Naukriprofileupdate;   // import your page class
 
-import Pages.Naukriprofileupdate;
+public class NakuriUpdateTest {
 
-public class NakuriUpdateTest 
-{
-	@Test
-   static void Nakuriupdate() throws Exception
-   {
-	   Naukriprofileupdate.NakuriChange();
-   }
+    @BeforeTest
+    public void setup() throws Exception {
+        // Kill existing Chrome processes before starting the test
+        // Runtime.getRuntime().exec("pkill -f chrome");
+        Thread.sleep(2000); // Small delay to ensure process is killed
+    }
+
+    @Test
+    public void Nakuriupdate() throws Exception {
+        Naukriprofileupdate.NakuriChange();
+    }
 }

6b4d5cf9e53dff260e4f1d63e9a673591ecc42dd
Krishna Mohan B <95032471+mohank1234@users.noreply.github.com>
2025-08-18 08:09:59 +0530

    Update BaseTest.java

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -9,20 +9,20 @@ import java.io.File;
 import java.nio.file.*;
 
 public class BaseTest {
-    protected WebDriver driver;
+    // protected WebDriver driver;
 
-    @AfterMethod
-    public void takeScreenshotOnFailure(ITestResult result) {
-        if (ITestResult.FAILURE == result.getStatus()) {
-            try {
-                File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-                Path dest = Paths.get("target/screenshots/failure_" + result.getName() + ".png");
-                Files.createDirectories(dest.getParent());
-                Files.copy(src.toPath(), dest, StandardCopyOption.REPLACE_EXISTING);
-                System.out.println("Failure screenshot saved at: " + dest.toAbsolutePath());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
+    // @AfterMethod
+    // public void takeScreenshotOnFailure(ITestResult result) {
+    //     if (ITestResult.FAILURE == result.getStatus()) {
+    //         try {
+    //             File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+    //             Path dest = Paths.get("target/screenshots/failure_" + result.getName() + ".png");
+    //             Files.createDirectories(dest.getParent());
+    //             Files.copy(src.toPath(), dest, StandardCopyOption.REPLACE_EXISTING);
+    //             System.out.println("Failure screenshot saved at: " + dest.toAbsolutePath());
+    //         } catch (Exception e) {
+    //             e.printStackTrace();
+    //         }
+    //     }
+    // }
 }
